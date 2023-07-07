498769611817de2f8f57d1899da217b3cb567169
AlexEkdahl <77007088+AlexEkdahl@users.noreply.github.com>
2023-07-07 22:59:16 +0200

    test httpserver

---
+++ server/httpserver_test.go
@@ -0,0 +1,49 @@
+package server
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"testing"
+	"time"
+)
+
+func TestHTTPServer(t *testing.T) {
+	// Start the HTTP server
+	tunnelStore := NewTunnel()
+	server := NewHTTPServer(tunnelStore, "8080")
+
+	errCh := make(chan error, 1)
+	go func() {
+		errCh <- server.StartHTTPServer(context.Background())
+	}()
+
+	// Wait for the server to start
+	time.Sleep(time.Second)
+
+	// Check for server start error
+	select {
+	case err := <-errCh:
+		if err != nil {
+			t.Fatalf("Failed to start HTTP server: %s", err)
+		}
+	default:
+	}
+
+	// Make a GET request
+	resp, err := http.Get("http://localhost:8080/?id=testID")
+	if err != nil {
+		t.Fatalf("Failed to make GET request: %s", err)
+	}
+	defer resp.Body.Close()
+
+	// Check the response
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatalf("Failed to read response body: %s", err)
+	}
+
+	if resp.StatusCode != http.StatusNotFound || string(body) != "Not Found\n" {
+		t.Errorf("Expected status 404 Not Found, got %d %s", resp.StatusCode, body)
+	}
+}
