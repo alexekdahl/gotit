c99419e12d48d0dd555a92773ce9e5d063b1a12c
AlexEkdahl <alexanderekdahl@me.com>
2023-09-01 08:31:55 +0200

    wip

--- server/httpserver.go
+++ server/httpserver.go
@@ -2,24 +2,43 @@ package server
 
 import (
 	"context"
+	"html/template"
+	"io"
 	"net/http"
+	"path/filepath"
 	"time"
 
 	"github.com/AlexEkdahl/gotit/utils/logger"
 )
 
+type templateRenderer struct {
+	template *template.Template
+}
+
+func (t *templateRenderer) render(w io.Writer, name string, data interface{}) error {
+	return t.template.ExecuteTemplate(w, name, data)
+}
+
 type HTTPServer struct {
-	tunnelStorer TunnelStorer
-	http         http.Server
-	logger       logger.Logger
+	tunnelStorer     TunnelStorer
+	templateRenderer *templateRenderer
+	http             http.Server
+	logger           logger.Logger
 }
 
 func NewHTTPServer(tunnelStorer TunnelStorer, logger logger.Logger, port string) *HTTPServer {
+	tmplPath := filepath.Join("www/templates/index.html")
+	t, err := template.ParseFiles(tmplPath)
+	if err != nil {
+		return nil
+	}
+
 	s := &HTTPServer{
 		tunnelStorer: tunnelStorer,
-		http: http.Server{
-			Addr: ":" + port,
+		templateRenderer: &templateRenderer{
+			template: t,
 		},
+		http:   http.Server{Addr: ":" + port},
 		logger: logger,
 	}
 
@@ -49,6 +68,16 @@ func (s *HTTPServer) StartHTTPServer(ctx context.Context) error {
 }
 
 func (s *HTTPServer) handleHTTPReq(w http.ResponseWriter, r *http.Request) {
+	s.logger.Debug("URL: " + r.URL.Path)
+	if r.URL.Path == "/" {
+		if err := s.templateRenderer.render(w, "index.html", nil); err != nil {
+			s.logger.Error(err)
+			return
+		}
+
+		return
+	}
+
 	id := r.URL.Query().Get("id")
 	tunnelChan, ok := s.tunnelStorer.Get(id)
 	if !ok {
